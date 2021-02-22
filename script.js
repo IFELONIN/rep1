ae87694db23010572f9209505b8c9e702bec376c
ILIA FELONIN <felonin@ILIAs-MacBook-Air.local>
2021-02-23 02:38:44 +0300

    Added my project

---
+++ script.js
@@ -0,0 +1,30 @@
+// Задача №10 ——> Сделает верхний регистр.
+
+//function bigString(str) {
+//  return str.toUpperCase()
+// }
+//  console.log(bigString("Привет, как дела?"));
+///
+
+
+
+
+
+// let newNumber = 3;
+//  function findeNumber(newNumber) {
+//   if (newNumber % 2 === 0) {
+//    return "Число чётное";
+//    }
+//    else {
+//    return "Число нечётное";
+//    }
+//  }
+//  console.log(findeNumber(newNumber));
+
+f
+
+
+
+
+
+let function = 3 
